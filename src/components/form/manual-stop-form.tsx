651ad511baf669f06f8b92e6a18a16e20d273fa6
Evan Holmes <ivur.nave@gmail.com>
2024-08-12 21:58:20 -0500

    input stop button

--- src/components/form/manual-stop-form.tsx
+++ src/components/form/manual-stop-form.tsx
@@ -1,33 +1,43 @@
-'use client'
+'use client';
 
-import { FormEvent, useState } from "react";
+import { useRef, useState } from 'react';
 
 export function ManualStopForm(props: { onSubmit: (stopId: string) => void }) {
-
     const [stopInputValue, setStopInputValue] = useState<string>('');
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const formPlaceholder = 'Enter a stop number';
 
-    const handleFormSubmit = (event : React.FormEvent<HTMLFormElement>) => {
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        props.onSubmit(stopInputValue);
-    }
+        if (stopInputValue) {
+            props.onSubmit(stopInputValue);
+            setStopInputValue('');
+        } else if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
 
     const handleInputChange = (event: React.FormEvent<HTMLInputElement>) => {
         setStopInputValue(event.currentTarget.value);
-    }
+    };
 
     return (
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleFormSubmit} className="flex space-x-4">
             <input
+                ref={inputRef}
                 type="text"
                 className="form-input"
                 placeholder={formPlaceholder}
                 onChange={handleInputChange}
                 value={stopInputValue}
             />
-            <button type="submit" />
+            <button
+                type="submit"
+                className="btn bg-slate-200 hover:bg-slate-300"
+            >
+                Add stop
+            </button>
         </form>
-    )
-
+    );
 }
